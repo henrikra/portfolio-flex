e4e128b5e128794367a9ad70031ec1e53712606b
Henrik Raitasola <heka.fof@gmail.com>
2016-02-09 09:19:40 +0200

    Added first version of navbar and intro.

---
+++ page-home.php
@@ -0,0 +1,16 @@
+<?php
+/*
+	Template Name: Home Page
+*/
+?>
+<header class="intro">
+	<div class="intro--overlay">
+		<div class="intro--container">
+			<div class="intro--catchphrase">Tekemässä nettisivuja, jotta sinun ei tarvitse</div>
+			<a href="#" class="cta">Katso töitäni</a>
+			<a href="#" class="cta-secondary">Ota yhteyttä</a>
+		</div>
+	</div>
+	
+</header>
+<div style="height:2000px"></div>
--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,30 @@
 <header class="banner">
+  <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">Minusta</a></li>
+          <li><a href="#">Taidot</a></li>
+          <li><a href="#">Portfolio</a></li>
+          <li><a href="#">Työkokemus</a></li>
+          <li><a href="#">Ota yhteyttä</a></li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </nav>
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
     <nav class="nav-primary">
       <?php
       if (has_nav_menu('primary_navigation')) :
