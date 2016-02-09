5f7c48cbc6bb5b082b987178d8c2c0a96b647e91
Henrik Raitasola <heka.fof@gmail.com>
2016-02-09 10:34:09 +0200

    Added skills section.

--- page-home.php
+++ page-home.php
@@ -13,4 +13,32 @@
 	</div>
 	
 </header>
+<section class="skills">
+	<div class="container">
+		<h3>Mitä teen?</h3>
+		<div class="row">
+			<div class="col-sm-4">
+				<div class="service">
+					<i class="service--icon fa fa-desktop"></i>
+					<h4 class="service--heading">Suunnittelu</h4>
+					<p class="service--description">Aliquam nunclorem conubia. Quisque ultricies cras ad. Molestie nunclorem. Fermentum eiusmod. Quisque ultricies cras ad. Molestie nunclorem.</p>
+				</div>
+			</div>
+			<div class="col-sm-4">
+				<div class="service">
+					<i class="service--icon fa fa-code"></i>
+					<h4 class="service--heading">Koodausta</h4>
+					<p class="service--description">Aliquam nunclorem conubia. Quisque ultricies cras ad. Molestie nunclorem. Fermentum eiusmod. Quisque ultricies cras ad. Molestie nunclorem.</p>
+				</div>
+			</div>
+			<div class="col-sm-4">
+				<div class="service">
+					<i class="service--icon fa fa-server"></i>
+					<h4 class="service--heading">Tuotantoon</h4>
+					<p class="service--description">Aliquam nunclorem conubia. Quisque ultricies cras ad. Molestie nunclorem. Fermentum eiusmod. Quisque ultricies cras ad. Molestie nunclorem.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
 <div style="height:2000px"></div>
