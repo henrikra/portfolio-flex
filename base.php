50eef355d8127474d3a7fc3238bc1bf5c56c493d
Henrik Raitasola <heka.fof@gmail.com>
2016-02-11 14:44:36 +0200

    Added scroll spy to navbar links.

--- base.php
+++ base.php
@@ -8,7 +8,7 @@ use Roots\Sage\Wrapper;
 <!doctype html>
 <html <?php language_attributes(); ?>>
   <?php get_template_part('templates/head'); ?>
-  <body <?php body_class(); ?>>
+  <body <?php body_class(); ?> data-spy="scroll" data-target="#navbar" data-offset="71">
     <!--[if IE]>
       <div class="alert alert-warning">
         <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
